31359f597fee6964fb73110c26cb304531b1e87b
Entwoane <antoine.musci@gmail.com>
2025-01-14 20:27:04 +0100

    Correcting format and variable storage

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range (97, 123):
-    if i == 101 or i == 113:
-        continue
-    print(chr(i), end="")
-    
+print("".join("{:c}".format(i) for i in range(97, 123)
+              if i not in (101, 113)), end="")
