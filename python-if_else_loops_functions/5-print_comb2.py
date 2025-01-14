a82c70a20dc40731202cdd6c2bf49d7c87d588c3
Entwoane <antoine.musci@gmail.com>
2025-01-14 20:31:22 +0100

    Minor adjustment

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):
-        if i == 8 and j == 9:
-            print("{:02d}".format(i * 10 + j))
-        else:
-            print("{:02d}, ".format(i * 10 + j), end="")
+for i in range(100):
+    if i != 99:
+        print("{:02d}, ".format(i), end="")
+    else:
+        print("{:02d}".format(i))
