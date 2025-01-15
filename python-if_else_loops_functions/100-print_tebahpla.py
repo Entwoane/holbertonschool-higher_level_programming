3101abeace8070bc0d80b712ac16bb116616a2bf
Entwoane <antoine.musci@gmail.com>
2025-01-15 11:47:10 +0100

    Trying with only one print, using join to concatenate all the characters

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,2 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    if (122 - i) % 2 == 0:
-        print(chr(i), end='')
-    else:
-        print(chr(i - 32), end='')
+print("".join(chr(122 - i - 32 * (i % 2)) for i in range(26)), end="")
