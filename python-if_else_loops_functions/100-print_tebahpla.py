40520f986b8358cde6d312c473272ea2ff23123f
Entwoane <antoine.musci@gmail.com>
2025-01-16 18:08:21 +0100

    Print tebalpha

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-for i in range(122, 96, -1):
-    if (122 - i) % 2 == 0:
-        print(chr(i), end='')
-    else:
-        print(chr(i - 32), end='')
-=======
 for i in range(26):
     print("{:c}".format(122 - i - (i % 2 * 32)), end="")
->>>>>>> 49bab1a8039503eb3848742a0dafd30c4916693f
