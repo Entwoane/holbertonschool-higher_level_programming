5112d1b05a34489b01e74d63c3915a3a1e5bde44
Entwoane <antoine.musci@gmail.com>
2025-01-19 13:07:10 +0100

    Correcting argc

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -3,22 +3,24 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, div, mul
 
-    num_args = len(sys.argv) - 1
-    if num_args > 4:
+    argc = len(sys.argv) - 1
+    if argc != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
+
+    a = int(sys.argv[1])
     operator = sys.argv[2]
+    b = int(sys.argv[3])
+    
     if operator not in ["+", "-", "*", "/"]:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    if operator == "+":
+    if operator == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
-    elif operator == "-":
+    elif operator == '-':
         print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif operator == "*":
+    elif operator == '*':
         print("{} * {} = {}".format(a, b, mul(a, b)))
     else:
         print("{} / {} = {}".format(a, b, div(a, b)))
