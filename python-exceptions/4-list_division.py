064ef149a0ee85a4a13acd9de2cd24f8f36ffda6
Entwoane <antoine.musci@gmail.com>
2025-01-24 16:26:00 +0100

    Correct Pycodestyle

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -5,19 +5,20 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             if i >= len(my_list_1) or i >= len(my_list_2):
                 raise IndexError
-            
+
             dividend = my_list_1[i]
             divisor = my_list_2[i]
-            
-            if not (isinstance(dividend, (int, float)) and isinstance(divisor, (int, float))):
+
+            if not (isinstance(dividend, (int, float))
+                    and isinstance(divisor, (int, float))):
                 raise TypeError
-            
+
             if divisor == 0:
                 raise ZeroDivisionError
-            
+
             div_result = dividend / divisor
             result.append(div_result)
-        
+
         except IndexError:
             print("out of range")
             result.append(0)
@@ -29,5 +30,5 @@ def list_division(my_list_1, my_list_2, list_length):
             result.append(0)
         finally:
             pass
-    
+
     return result
