de1ef0e9eaf25bc3ac5f6d0646fb7d0f34944318
Entwoane <antoine.musci@gmail.com>
2025-01-25 15:35:02 +0100

    Module documentation (?) + correct pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
+"""Module for add integer method"""
+
+
 def add_integer(a, b=98):
     """
     Adds two integers
-    
+
     Args:
         a: first number
         b=98: second number. Default to 98.
-    
+
     Returns:
         int: sum of a and b
-    
+
     Raises:
         TypeError: If a or b is not an integer
     """
