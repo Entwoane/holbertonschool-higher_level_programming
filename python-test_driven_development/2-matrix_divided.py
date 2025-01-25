ee858c948b3ce176506f6d7ece15512855e23b74
Entwoane <antoine.musci@gmail.com>
2025-01-25 16:42:15 +0100

    Add a missing space

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -22,7 +22,7 @@ def matrix_divided(matrix, div):
         not all(isinstance(row, list) for row in matrix) or
         not all(isinstance(elem, (int, float))
                 for row in matrix for elem in row)):
-        raise TypeError("matrix must be a matrix (list of lists) of"
+        raise TypeError("matrix must be a matrix (list of lists) of "
                         "integers/floats")
 
     if not all(len(row) == len(matrix[0]) for row in matrix):
