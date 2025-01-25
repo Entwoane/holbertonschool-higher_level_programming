e739e64bb8fb8c1d383f82e177d5abb1af186ce8
Entwoane <antoine.musci@gmail.com>
2025-01-25 18:05:48 +0100

    Added boolean check

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -17,7 +17,7 @@ def print_square(size):
         ValueError: if size is less than 0
         TypeError: if size is a float and less than 0
     """
-    if not isinstance(size, int):
+    if type(size) is bool or not isinstance(size, int):
         raise TypeError("size must be an integer")
 
     if size < 0:
