12bd9bfe8fcd72902c2c64356b801040d38e8c45
Entwoane <antoine.musci@gmail.com>
2025-01-29 16:59:31 +0100

    Simple rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module for defining and working with rectangle object"""
+
+
+class Rectangle:
+    """
+    Class representing a rectangle shape
+    This class allows creation and manipulation of rectangle
+    objects with a specified size
+    """
+    pass
