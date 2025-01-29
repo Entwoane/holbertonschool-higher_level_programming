9c5bad8b5ff9987d885cce5978c4d886ac156499
Entwoane <antoine.musci@gmail.com>
2025-01-29 16:32:23 +0100

    Empty class Square that defines a square

---
+++ python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module for defining and working with square object"""
+
+
+class Square:
+    """
+    Class representing a square shape
+    This class allows creation and manipulation of square
+    objects with a specified size
+
+    """
+    pass
