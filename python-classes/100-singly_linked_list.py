c28d7f4367c90e3df0fbbfd2c6dc77a400d97108
Entwoane <antoine.musci@gmail.com>
2025-01-30 19:04:13 +0100

    Added documentation

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -1,39 +1,67 @@
 #!/usr/bin/python3
 
-
 class Node:
 
     def __init__(self, data, next_node=None):
+        """
+        Initialize a new Node.
+
+        Args:
+            data (int): The data stored in the node.
+            next_node (Node): The next node in the list.
+        """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
+        """Getter for the data attribute."""
+
         return self.__data
 
     @data.setter
     def data(self, value):
+        """
+        Setter for the data attribute.
+
+        Raises:
+            TypeError: If value is not an integer.
+        """
+
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
+        """Getter for the next_node attribute."""
+
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
+        """
+        Setter for the next_node attribute.
+        Raises:
+            TypeError: If value is not None or a Node object.
+        """
+
         if value is not None and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 
 class SinglyLinkedList:
+    """Represents a singly linked list."""
 
     def __init__(self):
+        """Initialize an empty singly linked list."""
+
         self.__head = None
 
     def __str__(self):
+        """Return a string representation of the list."""
+
         result = []
         current = self.__head
         while current:
@@ -42,6 +70,13 @@ class SinglyLinkedList:
         return '\n'.join(result)
 
     def sorted_insert(self, value):
+        """
+        Insert a new Node into the correct sorted position in the list.
+
+        Args:
+            value (int): The value to be inserted.
+        """
+
         new_node = Node(value)
 
         if not self.__head or self.__head.data >= value:
