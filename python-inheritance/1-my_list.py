1fb4ddb241acb1d5785bc1de1ad325daea8c5c22
Entwoane <antoine.musci@gmail.com>
2025-02-05 11:58:09 +0100

    Docstring

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-""" Module for print_sorted function"""
+""" Module for MyList class"""
 
 
 class MyList(list):
     """
     A custom list subclass that provides additional sorting functionality.
 
-    Args:
-        list (int): list of integers
+    Public instance method:
+        - print_sorted(self): Prints the list in ascending order.
     """
     def print_sorted(self):
         """
