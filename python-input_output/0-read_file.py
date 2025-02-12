b46bbcce6b07a6e7656dfd10a6076172623580a4
Entwoane <antoine.musci@gmail.com>
2025-02-12 15:58:15 +0100

    Read file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module provides utility functions for file operations.
+The `read_file` function reads the content of a file
+and prints it to the console.
+It is designed to handle text files encoded in UTF-8.
+"""
+
+
+def read_file(filename=""):
+    """
+    Read a file and print its content
+
+    Args:
+        filename (str, optional): The name of the file to read. Defaults to "".
+    """
+    with open(filename, encoding="utf-8") as file:
+        read_file = file.read()
+    print(read_file)
