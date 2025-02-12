bf0f01c064b6d4bb88e29c50ba777168ce4648be
Entwoane <antoine.musci@gmail.com>
2025-02-12 16:00:05 +0100

    Student to disk and reload

---
+++ python-input_output/11-student.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+This module defines a `Student` class with attributes
+for first name, last name, and age.
+
+The `Student` class includes methods to retrieve
+a dictionary representation of its attributes, with an option to filter
+specific attributes, and to update its attributes from a dictionary.
+"""
+
+
+class Student:
+    """
+    A class that defines a student with first name, last name, and age.
+    Provides a method to retrieve a dictionary representation of the instance.
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize a new Student instance.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieve a dictionary representation of the Student instance.
+
+        Returns:
+            dict: A dictionary containing all attributes of the instance.
+            If attrs is None or not a list of strings,
+            all attributes are returned.
+        """
+        if (isinstance(attrs, list) and all(isinstance(item, str)
+                                            for item in attrs)):
+            return {key: value for key, value in self.__dict__.items()
+                    if key in attrs}
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        Replace all attributes of the Student instance
+        with values from a dictionary.
+
+        Args:
+            json (dict): A dictionary where keys are attribute names
+            and values are the new values for those attributes.
+        """
+        for key, value in json.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
