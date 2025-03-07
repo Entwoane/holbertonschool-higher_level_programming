603fcd63b54e12c9b5b5ac5ada4f39f02fa8f26e
Entwoane <antoine.musci@gmail.com>
2025-03-07 15:57:47 +0100

    Super bien j'adore c'est tellement cool

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,31 +1,37 @@
 #!/usr/bin/python3
 """
-Module to connect to a MySQL database and list all states ordered by ID.
-
-Usage:
-    python3 script.py <mysql_username> <mysql_password> <mysql_database>
+Script that lists all State object from the db hbtn_0e_6_usa
 """
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-import sys
+
 
 if __name__ == "__main__":
+
+    if len(sys.argv) != 4:
+        print("Usage: {} <mysql username> <mysql password> <database name>"
+              .format(sys.argv[0]))
+        sys.exit(1)
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
     engine = create_engine(
-        f'mysql+mysqldb://{username}:{password}@localhost/'
-        f'{database}',
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(username, password, database),
         pool_pre_ping=True
     )
-    Base.metadata.create_all(engine)
+
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).order_by(State.id).all()
+
+    states = session.query(State).order_by(State.id.asc()).all()
+
     for state in states:
         print(f"{state.id}: {state.name}")
 
