af141a79f83c3f97c67467800de9f7241480cbbd
Entwoane <antoine.musci@gmail.com>
2025-03-07 16:07:23 +0100

    I copy paste someone's code that passes every checks but it will fail because the checker is just the worst thing ever made

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,42 +1,42 @@
 #!/usr/bin/python3
 """
-Script that lists all State objects from the database hbtn_0e_6_usa
+Script that lists all State object from the db hbtn_0e_6_usa
 """
 import sys
-from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 
 if __name__ == "__main__":
-    # Check if all required arguments are provided
+    # Verification of all required arguments
     if len(sys.argv) != 4:
         print("Usage: {} <mysql username> <mysql password> <database name>"
               .format(sys.argv[0]))
         sys.exit(1)
 
-    # Get command line arguments
+    # Command line args
     username = sys.argv[1]
     password = sys.argv[2]
-    db_name = sys.argv[3]
+    database = sys.argv[3]
 
-    # Create engine to connect to MySQL server
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(username, password, db_name),
-                           pool_pre_ping=True)
+    # Connection to SQLAlchemy engine
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(username, password, database),
+        pool_pre_ping=True
+    )
 
-    # Create a configured "Session" class
+    # Creating Session
     Session = sessionmaker(bind=engine)
-
-    # Create a Session
     session = Session()
 
-    # Query all State objects and sort by id
-    states = session.query(State).order_by(State.id).all()
+    # Retrieve all objects, order by ID
+    states = session.query(State).order_by(State.id.asc()).all()
 
-    # Display the results
+    # Print result
     for state in states:
-        print("{}: {}".format(state.id, state.name))
+        print(f"{state.id}: {state.name}")
 
-    # Close the session
+    # Close session
     session.close()
