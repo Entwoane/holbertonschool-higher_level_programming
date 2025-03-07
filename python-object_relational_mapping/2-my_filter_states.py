ec023afc817b0f42e6a245a9e4d026e2d321923a
Entwoane <antoine.musci@gmail.com>
2025-03-07 12:32:02 +0100

    First part with MySQLdb module

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+Module to connect to a MySQL database and retrieve states by name
+ordered by ID.
+
+Usage:
+    python3 script.py <mysql_username> <mysql_password>
+    <database_name> <state_name>
+"""
+import MySQLdb
+import sys
+
+
+def main():
+    """
+    Main function to handle command line arguments and database operations.
+
+    Retrieves states from the database where the name matches
+    the given state name, ordered by ID, and prints them.
+    """
+    if len(sys.argv) != 5:
+        print("Usage: {} <mysql username> <mysql password> <database name>"
+              .format(sys.argv[0]))
+        sys.exit(1)
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name_searched = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        password=mysql_password,
+        database=database_name
+    )
+
+    cur = db.cursor()
+    query = ("SELECT * FROM states WHERE name = '{}'"
+             "ORDER BY id ASC".format(state_name_searched))
+    cur.execute(query)
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(f"({row[0]}, '{row[1]}')")
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+Module to connect to a MySQL database and list all cities ordered by ID.
+
+Usage:
+    python3 script.py <mysql_username> <mysql_password> <database_name>
+"""
+import MySQLdb
+import sys
+
+
+def main():
+    """
+    Main function to handle command line arguments and database operations.
+
+    Retrieves all cities from the database, ordered by ID, and prints them.
+    """
+    if len(sys.argv) != 4:
+        print("Usage: {} <mysql username> <mysql password> <database name>"
+              .format(sys.argv[0]))
+        sys.exit(1)
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        password=mysql_password,
+        database=database_name
+    )
+
+    cur = db.cursor()
+    query = """SELECT cities.id, cities.name, states.name
+    FROM cities
+    JOIN states ON cities.state_id = state_id
+    ORDER BY cities.id ASC"""
+    cur.execute(query)
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(f"({row[0]}, '{row[1]}', '{row[2]}')")
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
