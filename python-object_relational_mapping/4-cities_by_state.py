f64b71c37579a4ccab372b9f0ac071d6441acb3d
Entwoane <antoine.musci@gmail.com>
2025-03-07 13:01:33 +0100

    Small typo in query

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -33,16 +33,18 @@ def main():
     )
 
     cur = db.cursor()
-    query = """SELECT cities.id, cities.name, states.name
+    query = """
+    SELECT cities.id, cities.name, states.name
     FROM cities
-    JOIN states ON cities.state_id = state_id
-    ORDER BY cities.id ASC"""
+    JOIN states ON cities.state_id = states.id
+    ORDER BY cities.id ASC
+    """
     cur.execute(query)
 
     rows = cur.fetchall()
 
     for row in rows:
-        print(f"({row[0]}, '{row[1]}', '{row[2]}')")
+        print(row)
 
     cur.close()
     db.close()
