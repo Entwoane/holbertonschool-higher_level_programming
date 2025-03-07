772f3f496e9862756aaa3778c1736f373de80933
Entwoane <antoine.musci@gmail.com>
2025-03-07 12:34:43 +0100

    Correct SELECT query

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -35,7 +35,7 @@ def main():
     )
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
 
     for row in rows:
