35a9977e26e10824fbe527ea06f12d6c179c2df8
Entwoane <antoine.musci@gmail.com>
2025-03-07 16:00:32 +0100

    What the fuck

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -25,10 +25,8 @@ if __name__ == "__main__":
         pool_pre_ping=True
     )
 
-
     Session = sessionmaker(bind=engine)
     session = Session()
-
 
     states = session.query(State).order_by(State.id.asc()).all()
 
